fd85a0a5da1d4e0e626b5f7cddeaabf7930731ff
Milan-Sharma-449 <milansharma449@gmail.com>
2022-11-13 10:28:06 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-
+import {Switch, Route} from 'react-router-dom'
 function App() {
   return (
-    <div> 
-      Hi!
-    </div>
+    <Switch>
+      <Route exact path="/">
+        This is home page
+      </Route>
+
+      <Route exact path="/starred">
+        This is starred
+      </Route>
+
+      <Route>
+        This is 404 page
+      </Route>
+    </Switch>
   );
 }
 
