e6ce6504125790e8ae8c8120a5404d7bd0c611ca
Milan-Sharma-449 <milansharma449@gmail.com>
2022-11-27 10:24:13 +0530

    Updates

---
+++ precache-manifest.099a80f55ea46cefc6e3e07ba6b0ac01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adfdb84f788c6811488d378022d3979",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "2ffee1805fddda4e0adf",
+    "url": "/box-office/static/css/main.3cd6ee85.chunk.css"
+  },
+  {
+    "revision": "bc352bdc33804c2b1ce4",
+    "url": "/box-office/static/js/2.9aa08058.chunk.js"
+  },
+  {
+    "revision": "c9c5f12326e730f6171c9e7086115877",
+    "url": "/box-office/static/js/2.9aa08058.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ffee1805fddda4e0adf",
+    "url": "/box-office/static/js/main.81dc6535.chunk.js"
+  },
+  {
+    "revision": "c850b6c555a3d841251e",
+    "url": "/box-office/static/js/runtime-main.e4c5afaf.js"
+  }
+]);
